e3d530c36f16c61bf7faeffc54e5ac5975ba1c95
mvujas <milos.vujasinovic.98@gmail.com>
2017-02-20 20:16:49 +0100

    changes on initialize script

--- init.sh
+++ init.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-cat init.sql > sqlite3 database.db
+if [ -z $1 ]
+then
+	echo You must enter password
+	exit 1
+fi
+
+PASSWORD=$1
+
+if [ ! -f database.db ]
+then
+	cat init.sql | sqlite3 database.db
+	echo Database initialized successfully
+fi
+
+SECRET_KEY=$(python init.py)
+FILE=settings.py
+echo \# ------CONFIG FILE------ > $FILE
+echo PASSWORD = \'$PASSWORD\' >> $FILE
+echo SECRET_KEY = $SECRET_KEY >> $FILE
+echo DATABASE = \'database.db\' >> $FILE
+echo New config file is initialized
--- settings.py
+++ settings.py
@@ -1,5 +1,4 @@
-import os
-
-PASSWORD = 'blabla' # password goes here
-SECRET_KEY = b'G\x98\xedW\xa3[\xde)@\xf3\xe7\x0c\xb3$\x0c\xf8hl\xfc\xdf\xa3S!W'
+# ------CONFIG FILE------
+PASSWORD = blabla
+SECRET_KEY = ≤eMá‡Õ∑ÔXáÍ˘Œ\πÚ‹`&é
 DATABASE = 'database.db'
